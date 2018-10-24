4f7b284d32a5ee16ea296753d67a96f1b10b0273
Gonza Cerbelli <gonza.cerbelli@gmail.com>
2018-10-24 16:10:04 -0300

    add atributes to user-model

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,59 @@
 'use strict';
 
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      email: {
-        type: DataTypes.STRING,
+      id: {
         allowNull: false,
-        unique: true
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
       },
-      password: {
+      firstName: {
+        required: true,
         allowNull: false,
         type: DataTypes.STRING
+      },
+      lastName: {
+        required: true,
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      email: {
+        required: true,
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        lowercase: true,
+        validate: {
+          isEmail: true,
+          notEmpty: true,
+          isWoloxDomain() {
+            if (
+              this.email.search(/^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(wolox)\.com$/g) !== 0
+            ) {
+              throw new Error('Domain of email is not valid.');
+            }
+          }
+        }
+      },
+      password: {
+        required: true,
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          is: {
+            args: ['/^[a-zA-Z0-9]+$/'],
+            msg: 'Password require alphanumeric value.'
+          },
+          len: {
+            args: { min: 8, max: 50 },
+            msg: 'Password length is not in this range.'
+          }
+        }
       },
       createdAt: {
         allowNull: false,
@@ -29,11 +63,23 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         type: DataTypes.DATE
       }
-    },
-    {}
+    }
   );
+  User.beforeCreate((user, options) => {
+    return bcrypt
+      .hash(user.password, 10)
+      .then(hash => {
+        user.password = hash;
+      })
+      .catch(err => {
+        throw new Error();
+      });
+  });
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
   User.associate = function(models) {
-    // associations can be defined here
+
   };
   return User;
 };
--- migrations/migrations/20181022192310-create-user.js
+++ migrations/migrations/20181022192310-create-user.js
@@ -2,39 +2,51 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      lastName: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      email: {
-        allowNull: false,
-        unique: true,
-        type: Sequelize.STRING
-      },
-      password: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+    return queryInterface.createTable(
+      'Users',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        firstName: {
+          required: true,
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        lastName: {
+          required: true,
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        email: {
+          required: true,
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+          lowercase: true,
+          validate: {
+            isEmail: true,
+            notEmpty: true
+          }
+        },
+        password: {
+          required: true,
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
       }
-    });
+    );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Users');

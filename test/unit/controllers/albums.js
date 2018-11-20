a0a1d04a408e41debc80b2b8e93d905224020978
Gonza Cerbelli <gonza.cerbelli@gmail.com>
2018-11-20 14:05:29 -0300

    change token album tests

--- test/unit/controllers/albums.js
+++ test/unit/controllers/albums.js
@@ -5,18 +5,21 @@ const _ = require('lodash'),
   chaiHttp = require('chai-http'),
   server = require('../../../app'),
   factory = require('factory-girl').factory,
-  mocks = require('../support/mocks');
+  mocks = require('../support/mocks'),
+  jwt = require('../../../app/tools/jwtToken');
 
 chai.use(chaiHttp);
 
 describe.only('Controller: Album GET, `src/controller/album`', () => {
   let userTest = {};
+  const token = jwt.createToken({ userId: 1 });
 
   beforeEach(done => {
     mocks.mockAlbums();
     factory.create('user').then(user => {
       user.reload();
       userTest = user.dataValues;
+      userTest.sessionToken = token;
       done();
     });
   });
@@ -25,26 +28,19 @@ describe.only('Controller: Album GET, `src/controller/album`', () => {
     it('should return the albums', done => {
       chai
         .request(server)
-        .post('/users/sessions')
+        .get('/albums')
         .send(userTest)
-        .then(res => {
-          expect(res).to.have.status(200);
-          expect(res.body).have.property('sessionToken');
-          chai
-            .request(server)
-            .get('/albums')
-            .send(res.body)
-            .then(response => {
-              expect(response).to.have.status(200);
-              expect(response.body.length).to.equal(2);
-              done();
-            });
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response.body.length).to.equal(2);
+          done();
         });
     });
   });
 
   context('When requesting with an invalid token', () => {
     it('should return invalid token message', done => {
+      userTest.sessionToken = 'invalid token';
       chai
         .request(server)
         .get('/albums')
